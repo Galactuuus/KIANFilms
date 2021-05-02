b9c006ab72fe774fe72a3c93e1ff053abc2600f7
TheJarki <melendez.jarki@gmail.com>
2021-05-02 21:13:18 +0200

    Revert "Merge branch 'develop' into resolving-warings"
    This reverts commit 3c3e93dc88d5c6553baa17c98352be49424031db, reversing
    changes made to 0b642f39f4c4f9d8b898e808debdc173ba37a579.

--- src/Services/fetchUserByEmail.js
+++ src/Services/fetchUserByEmail.js
@@ -1,11 +1,11 @@
 import cookies from 'js-cookies';
 
-const fetchUserByEmail = async (email, skip, limit) => {
+const fetchUserByEmail = async (email) => {
 
     const token = cookies.getItem('auth');
 
     try {
-        let users = await fetch(`http://localhost:4000/user/find?email=${email}&skip=${skip}&limit=${limit}`, {
+        let users = await fetch(`http://localhost:4000/user/find?email=${email}`, {
             method: 'GET',
             headers:
             {
